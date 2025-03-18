b465e8af0b24bb7c16c4a87cab131e2eec8805a4
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-03-18 21:34:48 +0100

    Add nanoid library for generating random element IDs Add react-icons library Create eduIsOpen state to control education section visibility Create addEduDetails state to display new education form Implement toggle functions for eduIsOpen and addEduDetails states Implement function to delete university/school entries

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import GeneralInfo from "./components/GeneralInfo";
 import CVHeader from "./components/CVHeader";
 import "./styles/styles.css";
+import CVEducation from "./components/CVEducation";
+import EducationInfo from "./components/EducationInfo";
+import { nanoid } from "nanoid";
+import { MdDelete } from "react-icons/md";
 
 export default function App() {
+  const [eduIsOpen, setEduIsOpen] = React.useState(false);
+  const [addEduDetails, setAddEduDetails] = React.useState(false);
+
   const [personalDetails, setPersonalDetails] = React.useState({
     fullName: "Lysander Crowe",
     email: "lysander.crowe@xgmail.com",
@@ -11,12 +18,46 @@ export default function App() {
     address: "789 Nightfall Lane, USA",
   });
 
+  const [eduDetails, setEduDetails] = React.useState([
+    {
+      id: nanoid(),
+      school: "Moonlight University",
+      degree: "Bachelor of Arts in Creative Media and Film",
+      startdate: "05/2020",
+      enddate: "08/2024",
+      location: "Dream City, USA",
+    },
+    {
+      id: nanoid(),
+      school: "Moonliaaaght University",
+      degree: "Bachelor of Arts in Creative Media and Film",
+      startdate: "05/2020",
+      enddate: "08/2024",
+      location: "Dream City, USA",
+    },
+  ]);
+
+  // this the function for the right icon of education text
+  const changeEduIsOpen = () => {
+    setEduIsOpen((prevDetails) => !prevDetails);
+  };
+
+  //  this is the function for the "PLUS" icon of education block
+  const changeAddEduDetails = () => {
+    setAddEduDetails((prevDetails) => !prevDetails);
+  };
+
   const changeFunction = (stateName) => (e) => {
     const { name, value } = e.target;
     stateName((prevDetails) => ({
       ...prevDetails,
       [name]: value,
     }));
+  };
+
+  const deleteUni = (id) => {
+    const updatedUni = eduDetails.filter((item) => item.id !== id);
+    setEduDetails(updatedUni);
   };
 
   return (
@@ -30,6 +71,23 @@ export default function App() {
             phone={personalDetails.phone}
             address={personalDetails.address}
           ></GeneralInfo>
+          <EducationInfo
+            changeEduIsOpen={changeEduIsOpen}
+            changeAddEduDetails={changeAddEduDetails}
+            addEduDetails={addEduDetails}
+            eduIsOpen={eduIsOpen}
+            setEduDetails={setEduDetails}
+            uniName={
+              <>
+                {eduDetails.map((edu) => (
+                  <div className="school-bar" key={edu.id}>
+                    <p>{edu.school}</p>
+                    <MdDelete onClick={() => deleteUni(edu.id)} />
+                  </div>
+                ))}
+              </>
+            }
+          ></EducationInfo>
         </div>
         <div className="cv-section">
           <CVHeader

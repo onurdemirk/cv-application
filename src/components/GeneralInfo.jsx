108241de9d002efe5ca4b8f8a85cb14bbb433439
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-03-18 21:34:07 +0100

    Make changes for React environment

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -4,7 +4,7 @@ export default function GeneralInfo(props) {
   return (
     <div className="general-info">
       <p className="title">Personal Details</p>
-      <label for="fullname">Full name</label>
+      <label htmlFor="fullname">Full name</label>
       <input
         type="text"
         id="fullname"
@@ -12,7 +12,7 @@ export default function GeneralInfo(props) {
         value={props.name}
         onChange={props.onChange}
       />
-      <label for="email">Email</label>
+      <label htmlFor="email">Email</label>
       <input
         type="email"
         id="email"
@@ -20,7 +20,7 @@ export default function GeneralInfo(props) {
         value={props.email}
         onChange={props.onChange}
       />
-      <label for="phone">Phone</label>
+      <label htmlFor="phone">Phone</label>
       <input
         type="text"
         id="phone"
@@ -28,7 +28,7 @@ export default function GeneralInfo(props) {
         value={props.phone}
         onChange={props.onChange}
       />
-      <label for="address">Adress</label>
+      <label htmlFor="address">Adress</label>
       <input
         type="text"
         id="address"

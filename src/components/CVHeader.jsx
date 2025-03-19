d665155d02a208befd6da7291826c7df20ec0404
onurdemirk <96961148+onurdemirk@users.noreply.github.com>
2025-03-19 14:14:07 +0100

    Add icons for header elements (phone,mail,address)

--- src/components/CVHeader.jsx
+++ src/components/CVHeader.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { TfiEmail } from "react-icons/tfi";
+import { FiPhone } from "react-icons/fi";
+import { FaRegAddressCard } from "react-icons/fa6";
 
 export default function CVHeader(props) {
   return (
@@ -7,9 +10,15 @@ export default function CVHeader(props) {
         <h2>{props.name}</h2>
       </div>
       <div className="cv-header-details">
-        <p>{props.email}</p>
-        <p>{props.phone}</p>
-        <p>{props.address}</p>
+        <div className="header-details">
+          <TfiEmail className="header-button" /> <p>{props.email}</p>
+        </div>
+        <div className="header-details">
+        <FiPhone className="header-button" /> <p>{props.phone}</p>
+        </div>
+        <div className="header-details">
+        <FaRegAddressCard className="header-button" /><p>{props.address}</p>
+        </div>
       </div>
     </div>
   );
